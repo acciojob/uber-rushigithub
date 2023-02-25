71dc2895a120874f6d9771605fae35a41424b1c4
rushigithub <gaderushi89@gmail.com>
2023-02-25 22:12:38 +0530

    solution

---
+++ uber-p/uber-rushigithub/src/main/java/com/driver/controllers/AdminController.java
@@ -0,0 +1,48 @@
+package com.driver.controllers;
+
+import com.driver.model.Admin;
+import com.driver.model.Customer;
+import com.driver.model.Driver;
+import com.driver.services.impl.AdminServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	AdminServiceImpl adminServiceImpl;
+
+	@PostMapping("/register")
+	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){
+		adminServiceImpl.adminRegister(admin);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
+		Admin updatedAdmin = adminServiceImpl.updatePassword(adminId,password);
+		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/delete")
+	public void deleteAdmin(@RequestParam Integer adminId){
+		adminServiceImpl.deleteAdmin(adminId);
+	}
+
+	@GetMapping("/listOfCustomers")
+	public List<Customer> listOfCustomers() {
+		return adminServiceImpl.getListOfCustomers();
+	}
+
+	@GetMapping("/listOfDrivers")
+	public List<Driver> listOfDrivers() {
+		return adminServiceImpl.getListOfDrivers();
+	}
+}
