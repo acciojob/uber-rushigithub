52375aa8beee1d75b55d3a69597b37611eb69cec
rushigithub <gaderushi89@gmail.com>
2023-02-25 21:56:39 +0530

    solution

---
+++ uber-rushigithub/src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -0,0 +1,58 @@
+package com.driver.services.impl;
+
+import com.driver.model.Cab;
+import com.driver.repository.CabRepository;
+import com.driver.services.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.driver.model.Driver;
+import com.driver.repository.DriverRepository;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+
+	@Autowired
+	DriverRepository driverRepository3;
+
+	@Autowired
+	CabRepository cabRepository3;
+
+	@Override
+	public void register(String mobile, String password){
+		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+
+		// Setting Driver attributes
+		Driver driver = new Driver();
+		driver.setMobile(mobile);
+		driver.setPassword(password);
+
+		//Setting Cab attributes
+		Cab cab = new Cab();
+		cab.setAvailable(true);
+		cab.setPerKmRate(10);
+		cab.setDriver(driver);
+
+		driverRepository3.save(driver);
+
+	}
+
+	@Override
+	public void removeDriver(int driverId){
+		// Delete driver without using deleteById function
+		Driver driver = driverRepository3.findById(driverId).get();
+		driverRepository3.delete(driver);
+
+	}
+
+	@Override
+	public void updateStatus(int driverId){
+		//Set the status of respective car to unavailable
+		Driver driver = driverRepository3.findById(driverId).get();
+		Cab cab = driver.getCab();
+
+		cab.setAvailable(false);
+		cabRepository3.save(cab);
+
+	}
+}
